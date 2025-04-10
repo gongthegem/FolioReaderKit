109d08cebda7f3401ed709c7df15d6c4e96df474
“gongthegem” <gongchunyan@gongdeMacBook-Pro.local>
2025-04-10 16:36:49 +0100

    update Package

--- Package.swift
+++ Package.swift
@@ -1,16 +1,29 @@
 // swift-tools-version:5.8
-
 import PackageDescription
 
 let package = Package(
-	name: "FolioReaderKit",
-	dependencies: [
-		.Package(url: "https://github.com/ZipArchive/ZipArchive.git", majorVersion: 2, minor: 1),
-		.Package(url: "https://github.com/cxa/MenuItemKit.git", majorVersion: 3, minor: 0),
-		.Package(url: "https://github.com/zoonooz/ZFDragableModalTransition.git", majorVersion: 0, minor: 6),
-		.Package(url: "https://github.com/tadija/AEXML.git", majorVersion: 4, minor: 2),
-		.Package(url: "https://github.com/ArtSabintsev/FontBlaster.git", majorVersion: 4, minor: 0),
-		.Package(url: "https://github.com/fantim/JSQWebViewController.git", majorVersion: 6, minor: 1),
-		.Package(url: "https://github.com/realm/realm-cocoa.git", majorVersion: 3, minor: 1),
-	]
+    name: "FolioReaderKit",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "FolioReaderKit", targets: ["FolioReaderKit"])
+    ],
+    dependencies: [
+        // your needed dependencies (SwiftSoup, ZIPFoundation, etc.)
+    ],
+    targets: [
+        .target(
+            name: "FolioReaderKit",
+            dependencies: [
+                // reference .product(name: "ZIPFoundation", package: "ZIPFoundation"), etc.
+            ],
+            path: "Source" // or whatever the correct folder is
+        ),
+        .testTarget(
+            name: "FolioReaderKitTests",
+            dependencies: ["FolioReaderKit"],
+            path: "Tests"
+        )
+    ]
 )
